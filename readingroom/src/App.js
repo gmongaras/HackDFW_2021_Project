408858fdc3296a1f07c458157f3b62b18e838f8a
ostenloo <2ea64fad26@gmail.com>
2021-10-02 19:28:12 -0500

    homepage

--- readingroom/src/App.js
+++ readingroom/src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'; 
-// import {Homepage} from './pages/homepage'; 
+import {Homepage} from './pages/homepage'; 
 import {Login} from './pages/login'; 
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
@@ -9,7 +9,9 @@ function App()
       <div> 
         <Router>
           <Switch> 
+            <Route path="/home" component={Homepage}/>
             <Route path="/login" component={Login}/>
+            <Route path="/" component={Login}/>
           </Switch>
           </Router> 
       </div> 
--- readingroom/src/pages/homepage.jsx
+++ readingroom/src/pages/homepage.jsx
@@ -3,14 +3,40 @@ import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField'; 
 import { useFormControl } from '@mui/material/FormControl';
 import ReactDOM from 'react-dom';
-import { ReactComponent as ReadingRoomLogo } from '../logo/ReadingRoomLogo.svg';
+import Logo from '../logo/ReadingRoomLogoNoBackground.svg';
 import './homepage.css'; 
+import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
+import Grid from '@mui/material/Grid';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+
+const card = (
+    <div className = "recordings">
+        <p>Book</p>
+    </div>
+)
 
 export class Homepage extends React.Component { 
     render() {
         return(
             <>
-            
+                <div className = "containeryellow"> 
+                    <h1 style={{ fontSize: "3rem" }} > RECORDINGS </h1>
+                    <Grid container pacing ={4}> 
+                    <Grid xs = {12}>
+                        <Card variant="outlined">{card}</Card>
+                    </Grid> 
+                    <Grid xs = {12}>
+                            <Card variant="outlined">{card}</Card>
+                    </Grid>
+                    <Grid xs = {12}>
+                        <Card variant="outlined">{card}</Card>
+                    </Grid> 
+                </Grid>
+                </div> 
             </>
         )
     }
